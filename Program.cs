db8289d8abeaa86065412a5c89ba9c413793f11e
Anton Yavorskiy <63713478+AntonYavorskiy@users.noreply.github.com>
2021-06-22 20:00:18 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4_mas4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа 4. Массивы 4:Меняет знак всех элементов на противоположный.");
+            int v = 33;
+            Console.WriteLine("                      Порядковый номер по списку группы: 33");
+            Console.WriteLine("                                " + ((v % 9) + 1) + " Вариант");
+
+            Console.Write("Введите количество элементов:");
+            var len = int.Parse(Console.ReadLine());
+
+            var array = new int[len];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(i + " Элемент = ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            Console.WriteLine("Результат: ");
+
+            for (int i = 0; i < array.Length; i++)
+               
+
+                Console.Write(array[i] * -1 + ", ");
+
+
+
+
+                Console.ReadKey();
+        }
+    }
+}
